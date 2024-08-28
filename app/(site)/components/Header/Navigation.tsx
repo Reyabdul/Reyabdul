f9f316db1080acb196be1dea9d21e842ee9e22b9
Reyabdul <reyabdul@gmail.com>
2024-08-28 14:31:10 +0900

    fix header.css link error

--- app/(site)/components/Header/Navigation.tsx
+++ app/(site)/components/Header/Navigation.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import Link from "next/link";
 
@@ -7,7 +5,7 @@ import Link from "next/link";
 
 //styles
 import "../../../globals.css";
-import "./header.css";
+import "./Header.css";
 
 export const NavLinks = [
 
